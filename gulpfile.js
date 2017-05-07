89873dc888fa0deb276b2825b6646a9e3915015a
Khamitov Timur <ricomen@mail.ru>
2017-05-07 21:38:09 +0500

    add PostCSS

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,14 @@ const gulp           = require('gulp');
 const less           = require('gulp-less');
 const browserSync    = require('browser-sync');
 const concat         = require('gulp-concat');
-const uglify         = require('gulp-uglify');
 const cssnano        = require('gulp-cssnano');
 const rename         = require('gulp-rename');
 const del            = require('del');
 const imagemin       = require('gulp-imagemin');
 const pngquant       = require('imagemin-pngquant');
 const cache          = require('gulp-cache');
-const autoprefixer   = require('gulp-autoprefixer');
-// const postcss        = require('gulp-postcss');
+const postcss        = require('gulp-postcss');
+const autoprefixer   = require('autoprefixer');
 const plumber        = require('gulp-plumber');
 const csscomb        = require('gulp-csscomb');
 const spritesmith    = require('gulp.spritesmith');
@@ -18,60 +17,15 @@ const svgstore       = require('gulp-svgstore');
 const svgmin         = require('gulp-svgmin');
 const cheerio        = require('gulp-cheerio');
 const replace        = require('gulp-replace');
-const smartgrid      = require('smart-grid');
 const pug            = require('gulp-pug2');
-const eslint         = require('gulp-eslint');
 const notify         = require('gulp-notify');
-const babel          = require('gulp-babel');
 const sourcemaps     = require('gulp-sourcemaps');
 const gulpIf         = require('gulp-if');
+const cssgrace       = require('cssgrace')
+const mqpacker       = require("css-mqpacker");
 
 // const isDev = !process.env.NODE_ENV || process.env.NODE_ENV == 'dev';
 
-//ES-Linter
-gulp.task('lint', function () {
-  return gulp.src(['src/js/main.js','!node_modules/**'])
-  .pipe(eslint({
-    rules: {
-      'no-alert': 0,
-      'no-bitwise': 0,
-      'camelcase': 1,
-      'curly': 1,
-      'eqeqeq': 0,
-      'no-eq-null': 0,
-      'guard-for-in': 1,
-      'no-empty': 1,
-      'no-use-before-define': 0,
-      'no-obj-calls': 2,
-      'no-unused-vars': 0,
-      'new-cap': 1,
-      'no-shadow': 0,
-      'strict': 2,
-      'no-invalid-regexp': 2,
-      'comma-dangle': 2,
-      'no-undef': 1,
-      'no-new': 1,
-      'no-extra-semi': 1,
-      'no-debugger': 2,
-      'no-caller': 1,
-      'semi': 1,
-      'quotes': 0,
-      'no-unreachable': 2
-    },
-    globals: ['$'],
-    envs: ['node']
-    }))
-  .pipe(eslint.format())
-  .pipe(eslint.failAfterError())
-});
-
-gulp.task('babel', function() {
-  return gulp.src('src/js/es2015/*.js')
-    .pipe(babel({
-        presets: ['es2015']
-    }))
-    .pipe(gulp.dest('src/js/'));
-});
 
 //SVG-спрайт(собирает спрайт и кидает в корень img с расширением HTML)
 gulp.task('svgSprite', function () {
@@ -93,7 +47,7 @@ gulp.task('svgSprite', function () {
         },
           parserOptions: { xmlMode: true }
     }))    
-    .pipe(rename('symbol-sprite.svg'))
+    .pipe(rename('symbol-sprite.html'))
     .pipe(gulp.dest('src/img/'));
 });
 
@@ -108,26 +62,24 @@ gulp.task('pug', function() {
 });
 
 //LESS-препроцессор
+let postCssPlugins = [
+  autoprefixer({browsers: ['last 2 version']}),
+  mqpacker({sort: true}),  
+];
 gulp.task('less', function() {
   gulp.src('src/less/style.less')
     .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe(sourcemaps.init())
     .pipe(less())
+    .pipe(postcss(postCssPlugins))
     .pipe(csscomb())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('src/css'))
 });
 
-//PostCSS
-// gulp.task('css', function () {
-//   return gulp.src('./src/*.css')
-//     .pipe(postcss())
-//     .pipe(gulp.dest('./dest'));
-// });
-
 //PNG-спрайт(кидает в корень img + css в less/blocks)
 gulp.task('sprite', function () {
-  var spriteData = gulp.src('src/img/icons/*.png')
+  var spriteData = gulp.src('src/img/icons/for-sprite/*.png')
   .pipe(spritesmith({
     imgName: '../img/sprite.png',
     cssName: 'sprite.css',
@@ -163,17 +115,13 @@ gulp.task('scripts', function() {
 //Минимизируем css, добавляем префикс min
 gulp.task('css-libs', ['less'], function() {
   return gulp.src('src/css/style.css')
-      .pipe(autoprefixer({
-        browsers: ['last 8 versions'],
-        cascade: false
-      }))      
-      .pipe(cssnano())      
+      .pipe(cssnano())
       .pipe(rename({suffix: '.min'}))
       .pipe(gulp.dest('src/css'));
 });
 
 //Основной таск
-gulp.task('watch', ['browser-sync', 'less'], function() {
+gulp.task('watch', ['browser-sync', 'pug', 'less'], function() {
   gulp.watch('src/less/**/*.less', ['less']);
   gulp.watch('src/pug/**/*.pug', ['pug']);  
   gulp.watch('src/*.html', browserSync.reload);

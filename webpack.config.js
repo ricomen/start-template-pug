fadac56b7c8fe94e4f46a5811c0abda215e17164
Timur Khamitiov <ricomen@mail.ru>
2017-08-30 12:14:33 +0500

    Обновил зависимости + webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const PATHS = {
   src: path.join(__dirname, 'src'),
@@ -10,8 +11,39 @@ module.exports =  {
   output: {
     path: PATHS.build + /js/,
     filename: 'app.js'
-  },  
+  },
 
+  plugins: [
+    // new webpack.optimize.CommonsChunkPlugin({
+    //     name: 'vendor',
+    //     filename: '[name].js',
+    //     minChunks: Infinity
+    // }),
+    // uncomment in case of emergency code formatter need
+    // new PrettierPlugin({
+    //     printWidth: 80,
+    //     tabWidth: 4
+    // }),
+    new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+        beautify: false,
+        comments: false,
+        compress: {
+            sequences     : true,
+            booleans      : true,
+            loops         : true,
+            unused      : true,
+            warnings    : false,
+            drop_console: true,
+            unsafe      : true
+        }
+    })
+  ],
+  
   module: {
     rules: [
       {
